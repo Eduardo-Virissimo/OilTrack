854802acbf835f020905b1687c4f076f2c433193
guiqsassi <guiqsassi@gmail.com>
2024-11-12 13:52:04 -0300

    feat: filtros gerais

--- assets/js/tables.js
+++ assets/js/tables.js
@@ -3,12 +3,70 @@ baciaTable()
 
 var tbody = document.getElementById("tBody")
 var table = document.getElementById("tb")
-var select = document.getElementById("filterSelect").value
 
 var bacias;
 var pocos;
 
 var isBacia = true;
+
+function filtroSelect(){
+
+    var select = document.getElementById("filterSelect").value
+    console.log(select)
+    removerLinhas()
+    if(select != null ){
+
+        if(isBacia){
+            var baciasFiltradas = [];
+            
+            switch(select){
+                case "alphabet": bacias.sort((a,b) =>
+                    a.name.localeCompare(b.name)); break;
+                case "size": bacias.sort((a,b) =>
+                b.tamanho_km2 - a.tamanho_km2); break;
+                case "type": bacias.sort((a,b) =>
+                    a.tipo_de_fluido.localeCompare(b.tipo_de_fluido)); break;
+
+        }
+
+        console.log(bacias);
+        
+        bacias.forEach(bacia => {
+            
+            const newLine = tBody.insertRow()
+            newLine.insertCell(0).textContent = bacia.name
+            newLine.insertCell(1).textContent = bacia.estado
+            newLine.insertCell(2).textContent = bacia.cidade
+            newLine.insertCell(3).textContent = bacia.tamanho_km2
+            newLine.insertCell(4).textContent = bacia.tipo_de_fluido
+        })
+    }
+    else{           
+        console.log("Cai aqui")
+            switch(select){
+                case "alphabet": pocos.sort((a,b) =>
+                    a.name.localeCompare(b.name)); break;
+                case "size": pocos.sort((a,b) =>
+                b.tamanho_km2 - a.tamanho_km2); break;
+                case "type": pocos.sort((a,b) =>
+                    a.tipo_fluidos.localeCompare(b.tipo_fluidos)); break;
+    
+        }
+    
+        
+        pocos.forEach(poco => {
+            const newLine = tBody.insertRow()
+            newLine.insertCell(0).textContent = poco.name
+            newLine.insertCell(1).textContent = poco.estado
+            newLine.insertCell(2).textContent = poco.cidade
+            newLine.insertCell(3).textContent = poco.tamanho_km2
+            newLine.insertCell(4).textContent = poco.tipo_fluidos
+        })
+    
+    }
+    
+    }
+}
 
 function buscaNome(){
     var input = document.getElementById("filterInput").value
@@ -71,11 +129,13 @@ function removerLinhas() {
 function baciaTable(){
     isBacia = true;
 
+
     removerLinhas()
 fetch("../../mocks/bacias.json")
     .then(response => response.json()) 
     .then(data => {
-        bacias = data.bacias
+        bacias = data.bacias    
+
 
         console.log(data.bacias)        
         data.bacias.forEach(bacia => {
